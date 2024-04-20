9a375ab23eb5fda1f350dc5951ae1b860b8441af
Wiruj Suwanpramote <26286790-XtheWiz@users.noreply.replit.com>
2024-04-20 10:39:09 +0000

    Change code

--- hello.r
+++ hello.r
@@ -1,2 +1,15 @@
 # This is R Programming
 print('hello world')
+
+print("this is a second line")
+
+library(dplyr)
+df <- read_csv("filename.csv")
+
+df %>%
+  filter(column_a > 100)
+
+df %>%
+  select(1:10) %>%
+  filter(country == 'Thailand') %>%
+  arrange(-income)
